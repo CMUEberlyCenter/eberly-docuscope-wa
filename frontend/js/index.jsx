349a7857b12aabb052fd8156a5ea25877af3112e
MindtoEye <vvelsen@gmail.com>
2021-10-18 22:41:13 -0400

    Did a decent integration of the editor from OnTopic. Now we need to start hooking up the events and data management systems

--- frontend/js/index.jsx
+++ frontend/js/index.jsx
@@ -5,5 +5,5 @@ import ReactDOM from 'react-dom';
 import DocuScopeWA from './DocuScopeWA';
 import DocuScopeSandbox from './DocuScopeSandbox';
 
-//ReactDOM.render(<DocuScopeWA />, document.getElementById("content"));
-ReactDOM.render(<DocuScopeSandbox />, document.getElementById("content"));
+ReactDOM.render(<DocuScopeWA />, document.getElementById("content"));
+//ReactDOM.render(<DocuScopeSandbox />, document.getElementById("content"));
