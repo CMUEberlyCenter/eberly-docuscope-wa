eeb615b2b3a22c37aee98234152f770124f06a76
Michael Ringenberg <mringenb@andrew.cmu.edu>
2022-03-22 11:29:24 -0400

    Category expasion working.
    The expand/collapse category tree functionality now works.
    Checkboxes for highlighting available but do not work.

--- frontend/js/components/CategoryTree/CategoryTree.tsx
+++ frontend/js/components/CategoryTree/CategoryTree.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { Suspense } from 'react';
-import { Spinner } from 'react-bootstrap';
+import { Suspense, useState } from 'react';
+import { OverlayTrigger, Spinner, Tooltip } from 'react-bootstrap';
 import { CommonDictionary, CommonDictionaryTreeNode, useCommonDictionary } from '../../service/common-dictionary.service';
 import { gen_patterns_map, TaggerResults, useTaggerResults } from '../../service/tagger.service';
-import './CategoryTree.css';
+import './CategoryTree.scss';
 
 interface PatternData {
   pattern: string;
@@ -19,12 +19,14 @@ interface TreeNode {
 }
 
 const Patterns = (props: { category: string, data: PatternData[] }) => (
-  <table className='table table-sm patterns'>
-    <thead><tr><th scope='col'>Pattern</th><th scope='col'>Count</th></tr></thead>
-    <tbody>
-      {props.data && props.data.map((pat, i) => (<tr key={`${props.category}-row-${i}`}><td>{pat.pattern}</td><td>{pat.count}</td></tr>))}
-    </tbody>
-  </table>
+  <div className='table-responsive patterns-container ms-5'>
+    <table className='table table-sm patterns overflow-auto'>
+      <thead className='header'><tr><th scope='col'>Pattern</th><th scope='col' className='text-end'>Count</th></tr></thead>
+      <tbody>
+        {props.data && props.data.map((pat, i) => (<tr key={`${props.category}-row-${i}`}><td>{pat.pattern}</td><td className='text-end pe-3'>{pat.count}</td></tr>))}
+      </tbody>
+    </table>
+  </div>
 )
 
 function count_patterns(node: TreeNode): number {
@@ -35,13 +37,30 @@ function count_patterns(node: TreeNode): number {
   }
   return 0;
 }
-const CategoryNode = (props: { data: TreeNode }) => (
-  <li data-docuscope-category={props.data.id}><div>{props.data.label}<span className='material-icons comment mx-1' title={props.data.help}>comment</span>
-    {props.data.patterns.length > 0 ? (<span className='ms-auto'>{count_patterns(props.data)}</span>) : ''}</div>
-    {props.data.children.length > 0 ? (<ul>{props.data.children.map(sub => (<CategoryNode key={sub.id} data={sub} />))}</ul>) : ''}
-    {props.data.children.length === 0 && props.data.patterns.length > 0 ? (<Patterns category={props.data.id} data={props.data.patterns} />) : ''}
-  </li>
-);
+const CategoryNode = (props: { data: TreeNode }) => {
+  const [checked, setChecked] = useState(false);
+  const [expanded, setExpanded] = useState(true);
+  const check = `pattern-check-${props.data.id}`;
+  return (
+    <li data-docuscope-category={props.data.id} className='list-group-item'>
+      <span className='material-icons' onClick={() => setExpanded(!expanded)}>{expanded ? 'expand_more' : 'chevron_right'}</span>
+      <div className='form-check d-inline-flex align-items-baseline'>
+        <input className='form-check-input' id={check} type='checkbox' value={props.data.id} checked={checked} onChange={(e) => setChecked(e.currentTarget.checked)}/>
+        <label className='form-check-label' htmlFor={check}>
+          {props.data.label}
+          <OverlayTrigger overlay={<Tooltip>{props.data.help}</Tooltip>}>
+            <span className='material-icons comment mx-1'>comment</span>
+          </OverlayTrigger>
+        </label>
+        {props.data.patterns.length > 0 || !expanded ? (<span className='badge bg-primary rounded-pill fs-6'>{count_patterns(props.data)}</span>) : ''}
+      </div>
+      <div className={expanded ? '' : 'd-none'}>
+        {props.data.children.length > 0 ? (<ul className='list-group'>{props.data.children.map(sub => (<CategoryNode key={sub.id} data={sub} />))}</ul>) : ''}
+        {props.data.children.length === 0 && props.data.patterns.length > 0 ? (<Patterns category={props.data.id} data={props.data.patterns} />) : ''}
+      </div>
+    </li>
+  )
+};
 
 const CategoryTreeTop = () => {
   const common: CommonDictionary | null = useCommonDictionary();
@@ -57,7 +76,7 @@ const CategoryTreeTop = () => {
     });
     const data = common.tree.map(dfsmap);
     return (
-      <ul className='impressions-category-tree'>
+      <ul className='impressions-category-tree list-group'>
         {data && data.map((cat) => (<CategoryNode key={cat.id} data={cat} />))}
       </ul>
     );
