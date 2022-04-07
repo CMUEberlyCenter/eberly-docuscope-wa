348493340cf43b64cf643197fe9f6add40b03a5d
Michael Ringenberg <mringenb@andrew.cmu.edu>
2022-04-07 11:55:52 -0400

    Adds some error handling and reporting.

--- frontend/js/components/Impressions/Impressions.tsx
+++ frontend/js/components/Impressions/Impressions.tsx
@@ -1,8 +1,8 @@
 import { Subscribe } from "@react-rxjs/core";
 import * as React from "react";
-import { ProgressBar, Spinner } from "react-bootstrap";
+import { Alert, ProgressBar, Spinner } from "react-bootstrap";
 import { ErrorBoundary } from "react-error-boundary";
-import { useEditorState } from "../../service/editor-state.service";
+import { useEditorState, useEditorText } from "../../service/editor-state.service";
 import { useTaggerResults } from "../../service/tagger.service";
 import CategoryTree from "../CategoryTree/CategoryTree";
 import SunburstChart from "../SunburstChart/SunburstChart";
@@ -17,32 +17,35 @@ const ErrorFallback = (props: { error?: Error }) => (
 
 const Impressions = () => {
   const editing = useEditorState();
+  const text = useEditorText();
   const tagging = useTaggerResults();
   let content;
-  if (editing) {
+  if (editing || text.trim().length === 0 || tagging === null) {
     content = (<div className="alert alert-warning">
-      These tools is not valid when editing is enabled.
+      These tools is not valid when editing is enabled or there is no content.
     </div>);
-  } else if (!tagging || typeof(tagging) === 'number') {
-    content = (<div className="p-3"><ProgressBar striped variant="info" now={tagging??0} label={`${tagging??0}%`} /></div>);
+  } else if (typeof (tagging) === 'number') {
+    content = (<div className="p-3"><ProgressBar striped variant="info" now={tagging ?? 0} label={`${tagging ?? 0}%`} /></div>);
+  } else if (tagging.isError) {
+    content = (<Alert variant="danger"><Alert.Heading>Error:</Alert.Heading><p>{tagging.html_content}</p></Alert>);
   }
   return (
     <div className="impressions d-flex h-100 w-100 flex-column justify-content-start align-items-stretch">
       <TabTitle title="Manage Readers' Impressions" />
       {/*<div className="p-2 lh-lg"></div>*/}
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <React.Suspense fallback={<Spinner animation={"border"} />}>
-      <Subscribe>
-      {content ??
-        (<div className="impressions-content overflow-auto flex-grow-1 p-3">
-          <SunburstChart width={400} />
-          <h3 className="mt-2">Dictionary Categories</h3>
-          <CategoryTree />
-          {/*<iframe className="docuscopeframe" src={docuscope}></iframe>*/}
-        </div>)}
-      {/*<div className="impressions-detail"></div>*/}
-      </Subscribe>
-      </React.Suspense>
+        <React.Suspense fallback={<Spinner animation={"border"} />}>
+          <Subscribe>
+            {content ??
+              (<div className="impressions-content overflow-auto flex-grow-1 p-3">
+                <SunburstChart width={400} />
+                <h3 className="mt-2">Dictionary Categories</h3>
+                <CategoryTree />
+                {/*<iframe className="docuscopeframe" src={docuscope}></iframe>*/}
+              </div>)}
+            {/*<div className="impressions-detail"></div>*/}
+          </Subscribe>
+        </React.Suspense>
       </ErrorBoundary>
     </div>
   )
--- frontend/js/service/tagger.service.ts
+++ frontend/js/service/tagger.service.ts
@@ -1,5 +1,5 @@
 import { bind } from '@react-rxjs/core';
-import { filter, Observable, switchMap } from 'rxjs';
+import { catchError, filter, Observable, of, switchMap } from 'rxjs';
 import { editorState$, editorText } from './editor-state.service';
 
 interface PatternData {
@@ -16,14 +16,15 @@ export interface TaggerResults {
   html_content: string;
   tagging_time: number;
   patterns: PatternCategoryData[];
+  isError?: boolean;
 }
-/*const EmptyResults: TaggerResults = {
+const EmptyResults: TaggerResults = {
   doc_id: '',
   word_count: 0,
   html_content: '',
   tagging_time: 0,
   patterns: [],
-}*/
+}
 export function gen_patterns_map(
   res: TaggerResults
 ): Map<string, PatternData[]> {
@@ -40,7 +41,7 @@ interface Message {
 export function tag(text: string) {
   return new Observable<TaggerResults | number>(subscriber => {
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:8000/tag', true);
+    xhr.open('POST', 'http://localhost:8088/tag', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     let position = 0;
     xhr.addEventListener('progress', (ev) => {
@@ -121,9 +122,8 @@ export function tag(text: string) {
         subscriber.error(err);
       }
     });
-    xhr.addEventListener('error', (err) => {
-      console.error(err);
-      subscriber.error(err);
+    xhr.addEventListener('error', () => {
+      subscriber.error(new Error('Tagger is unavailable.'));
     });
     xhr.addEventListener('load', () => subscriber.complete());
     xhr.addEventListener('abort', () => subscriber.complete());
@@ -135,9 +135,11 @@ export function tag(text: string) {
 const tagEditorText = editorState$.pipe(
   filter(o => !o),
   switchMap(() => editorText.pipe(
-    filter(txt => !!txt),
+    filter(txt => txt.trim().length > 0),
     switchMap((text: string) => tag(text))
-  ))
+  )),
+  catchError((err: Error) =>
+    of({...EmptyResults, ...{isError: true, html_content: err.message}}))
 );
 
 export const [useTaggerResults, taggerResults$] = bind(
