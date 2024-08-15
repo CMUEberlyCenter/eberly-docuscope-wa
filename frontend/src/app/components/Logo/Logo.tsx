949663348f54c4962ef222e4ea4b335eb6b1b853
Michael Ringenberg <mringenb@andrew.cmu.edu>
2024-08-15 15:22:23 -0400

    [WIP] #51 adds myProse logo and favicon.

--- frontend/src/app/components/Logo/Logo.tsx
+++ frontend/src/app/components/Logo/Logo.tsx
@@ -1,15 +1,22 @@
 import { FC } from "react";
-import './Logo.scss';
-// import logo from "../../assets/logo.svg";
+// import './Logo.scss';
+import logo from "../../assets/logo.svg";
+import { Translation } from "react-i18next";
+
+// CSS logo
+// export const Logo: FC = () => (
+//   <div className="logo">
+//     <span className="prefix">my</span>
+//     <span className="main">Prose</span>
+//   </div>
+// );
 
 export const Logo: FC = () => (
-  <div className="logo">
-    <span className="prefix">my</span>
-    <span className="main">Prose</span>
-  </div>
+  <Translation>
+    {(t) => (<img
+      style={{ height: "1.75em" }}
+      src={logo}
+      alt={t("document.title")}
+    />)}
+  </Translation>
 );
-{/* <img
-style={{ height: "1.75em" }}
-src={logo}
-alt={tt("document.title")}
-/> */}
