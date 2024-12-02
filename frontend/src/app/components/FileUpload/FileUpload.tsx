49afd086759205c84bccb7010a330b8dcd811cdc
Michael Ringenberg <mringenb@andrew.cmu.edu>
2024-12-02 14:35:26 -0500

    [fix] prettier errors.

--- frontend/src/app/components/FileUpload/FileUpload.tsx
+++ frontend/src/app/components/FileUpload/FileUpload.tsx
@@ -47,7 +47,9 @@ export const FileUpload: FC<FileUploadProps> = ({
         <Button
           variant="primary"
           onClick={() => {
-            if (file) { onFile?.(file); }
+            if (file) {
+              onFile?.(file);
+            }
             onHide?.();
           }}
         >
