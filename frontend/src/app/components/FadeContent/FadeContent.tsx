7562449d1267b16d2a5c9562161ddee24b177341
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-02-26 14:20:21 -0500

    [fix] fixes aria attribute errors in FadeContent.

--- frontend/src/app/components/FadeContent/FadeContent.tsx
+++ frontend/src/app/components/FadeContent/FadeContent.tsx
@@ -1,4 +1,4 @@
-import { FC, HTMLProps, useEffect, useState } from "react";
+import { FC, HTMLProps, useEffect, useId, useState } from "react";
 import AnimateHeight, { Height } from "react-animate-height";
 import { Button } from "react-bootstrap";
 import css from "./FadeContent.module.scss";
@@ -19,17 +19,14 @@ export const FadeContent: FC<ToolFadeContentProps> = ({
   maxHeight = maxHeight ?? "auto";
   const [expanded, setExpanded] = useState(false);
   const [height, setHeight] = useState<Height>(minHeight);
+  const id = useId();
   useEffect(() => {
     setHeight(expanded ? maxHeight : minHeight);
   }, [expanded]);
 
   return (
     <article {...props}>
-      <AnimateHeight
-        height={height}
-        aria-expanded={expanded}
-        className={css["fade-content"]}
-      >
+      <AnimateHeight id={id} height={height} className={css["fade-content"]}>
         {/* Workaround for html string content. */}
         {htmlContent && (
           <div
@@ -41,7 +38,12 @@ export const FadeContent: FC<ToolFadeContentProps> = ({
         {children}
       </AnimateHeight>
       <div className="d-flex justify-content-around">
-        <Button variant="icon" onClick={() => setExpanded(!expanded)}>
+        <Button
+          variant="icon"
+          onClick={() => setExpanded(!expanded)}
+          aria-expanded={expanded}
+          aria-controls={id}
+        >
           ...
         </Button>
       </div>
