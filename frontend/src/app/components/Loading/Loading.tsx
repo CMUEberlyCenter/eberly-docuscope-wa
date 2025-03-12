9f37fc659ea13b31472de3b611186aa27f3e5e8a
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-03-12 13:26:07 -0400

    [fix] #173 adds text to Loading spinner.

--- frontend/src/app/components/Loading/Loading.tsx
+++ frontend/src/app/components/Loading/Loading.tsx
@@ -1,22 +1,30 @@
-import { FC } from "react";
+import { FC, HTMLProps } from "react";
 import { Translation } from "react-i18next";
 import DotSpinner from "../../assets/icons/6-dots-rotate.svg?react";
-import "./Loading.scss";
+import style from "./Loading.module.scss";
+import classNames from "classnames";
 
-export const Loading: FC = () => {
+export const Loading: FC<HTMLProps<HTMLDivElement>> = (className, ...props) => {
   return (
     <div
       role="status"
-      className="mx-auto loading w-100 h-100 position-relative"
+      className={classNames(
+        className,
+        `mx-auto loading w-100 h-100 position-relative`
+      )}
+      {...props}
     >
-      <div className="position-absolute top-50 start-50 translate-middle">
+      <div className="position-absolute top-50 start-50 translate-middle d-flex flex-column align-items-center">
         <DotSpinner
           aria-hidden="true"
-          className="text-primary loading-animate-spin"
+          className={`text-primary ${style["loading-animate-spin"]}`}
         />
         <Translation>
           {(t) => (
-            <span className="sr-only visually-hidden">{t("loading")}</span>
+            <>
+              <div className="text-center text-wrap">{t("loading")}</div>
+              <div className="text-center text-wrap fw-light">{t("wait")}</div>
+            </>
           )}
         </Translation>
       </div>
