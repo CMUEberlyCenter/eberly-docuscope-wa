cc3697b26a5c7746a80fc43f2e6a5ed2ee7be3da
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-03-19 13:18:55 -0400

    [fix] dump.sh

--- dump.sh
+++ dump.sh
@@ -1,1 +1,1 @@
-docker compose exec -T mongo mongodump myprose --authenticationDatabase admin --username $(cat secrets/mongo_user) --password $(cat secrets/mongo_pass) --archive > myprose.dump
+docker compose exec -T mongo mongodump --authenticationDatabase admin --username $(cat secrets/mongo_user) --password $(cat secrets/mongo_pass) --db myprose --archive > myprose.dump
