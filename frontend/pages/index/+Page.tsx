88ab20d0c27662e239054c9857bebe5722cf6263
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-06-24 11:29:35 -0400

    [chore] minor cleanup.

--- frontend/pages/index/+Page.tsx
+++ frontend/pages/index/+Page.tsx
@@ -1,12 +1,8 @@
 import type { FC } from "react";
 import CustomEditor from "../../src/app/components/Editor/CustomEditor";
-// import { useTranslation } from "react-i18next";
 
 export const Page: FC = () => {
-  // const {t} = useTranslation();
   return (
-    <>
-      <CustomEditor />
-    </>
+    <CustomEditor />
   );
 };
