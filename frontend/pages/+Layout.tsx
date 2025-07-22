065f9f0174d70fe08b5e36c25d3e005eff895673
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-07-22 09:58:50 -0400

    [chore] small bit of code refactoring.

--- frontend/pages/+Layout.tsx
+++ frontend/pages/+Layout.tsx
@@ -5,20 +5,14 @@ import "../src/app/index.scss";
 
 const translate = i18n.loadNamespaces("translation");
 
-const I18Next: FC<{ children: ReactNode }> = ({ children }) => {
-  use(translate);
-  return (
-    <I18nextProvider i18n={i18n} defaultNS={"translation"}>
-      {children}
-    </I18nextProvider>
-  );
-};
-
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  use(translate);
   return (
     <StrictMode>
       <div className="layout">
-        <I18Next>{children}</I18Next>
+        <I18nextProvider i18n={i18n} defaultNS={"translation"}>
+          {children}
+        </I18nextProvider>
       </div>
     </StrictMode>
   );
