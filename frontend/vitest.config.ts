5ae7130848408c6d80aaa5f72bfea604881d8342
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-07-28 08:33:11 -0400

    [fix] fixes vitest deprication warnings.

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import react from '@vitejs/plugin-react';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from 'vitest/config';
+
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
@@ -7,6 +15,26 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     coverage: {
-    }
+    },
+    projects: [
+      {
+        extends: "vite.storybook.config.ts",
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+          storybookTest({ configDir: path.join(dirname, ".storybook") }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        }
+      }
+    ]
   },
 });
