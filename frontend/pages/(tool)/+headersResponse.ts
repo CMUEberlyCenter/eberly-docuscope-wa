afee5040bbb4e5688f014636098050ca7e0e37d5
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2025-09-17 15:20:28 -0400

    [WIP] #220 adds missing headersResponse file.

---
+++ frontend/pages/(tool)/+headersResponse.ts
@@ -0,0 +1,10 @@
+import type { PageContextServer } from "vike/types";
+
+export async function headersResponse(_pageContext: PageContextServer) {
+  // pageContext.headersResponse.delete('Cross-Origin-Embedder-Policy');
+  // pageContext.headersResponse.delete('Cross-Origin-Resource-Policy');
+  return {
+    'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    // 'Cross-Origin-Embedder-Policy': 'require-corp',
+  };
+}
