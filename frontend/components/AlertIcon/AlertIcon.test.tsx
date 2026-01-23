07012b26c3cb1c510fdbd0340b32b368d5d68701
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-01-23 09:29:45 -0500

    [chore] adds ability to unit test components with svgr.

--- frontend/components/AlertIcon/AlertIcon.test.tsx
+++ frontend/components/AlertIcon/AlertIcon.test.tsx
@@ -3,8 +3,12 @@ import { describe, expect, test } from "bun:test";
 import { AlertIcon } from "./AlertIcon";
 
 describe("AlertIcon", () => {
-  test("render", () => {
+  test("render with show false", () => {
     render(<AlertIcon message="Alert" show={false} />);
     expect(screen.queryAllByTitle("Alert").length).toBe(0);
+  });
+  test("render with show true", () => {
+    render(<AlertIcon message="Alert" show={true} />);
+    expect(screen.queryAllByTitle("Alert").length).toBe(1);
   });
 });
