00015216e4af459ea0b5b973aa48b7ff760d9d99
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-01-26 11:11:36 -0500

    [WIP] more snapshot refactoring #280
    Fixes some parameters due to refactoring.
    Refactors onTopic review tools to reduce code duplication.

--- frontend/components/Review/ReviewContext.tsx
+++ frontend/components/Review/ReviewContext.tsx
@@ -1,3 +1,4 @@
+import { useMutation } from "@tanstack/react-query";
 import classNames from "classnames";
 import {
   createContext,
@@ -14,24 +15,24 @@ import {
 } from "react";
 import Alert from "react-bootstrap/esm/Alert";
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { Optional } from "../../src";
 import {
   Analysis,
   isErrorData,
+  OnTopicReviewData,
   OptionalReviewData,
   ReviewTool,
 } from "../../src/lib/ReviewResponse";
+import { WritingTask } from "../../src/lib/WritingTask";
 import {
   checkReviewResponse,
   ReviewErrorData,
 } from "../ErrorHandler/ErrorHandler";
+import { useFileText } from "../FileUpload/FileTextContext";
 import { Loading } from "../Loading/Loading";
 import { Summary } from "../Summary/Summary";
 import { ToolHeader } from "../ToolHeader/ToolHeader";
-import { useMutation } from "@tanstack/react-query";
-import { useFileText } from "../FileUpload/FileTextContext";
 import { useWritingTask } from "../WritingTaskContext/WritingTaskContext";
-import { WritingTask } from "../../src/lib/WritingTask";
-import { Optional } from "../../src";
 
 type ReviewContextState = {
   /** List of paragraph ids to highlight. */
@@ -214,6 +215,67 @@ export const ReviewToolCard: FC<
   );
 };
 
+export function useOnTopic() {
+  const [document] = useFileText();
+  const [review, setReview] =
+    useState<OptionalReviewData<OnTopicReviewData>>(null);
+  const abortControllerRef = useRef<AbortController | null>(null);
+  const dispatch = useReviewDispatch();
+
+  const mutation = useMutation({
+    mutationFn: async (data: { document: string }) => {
+      if (abortControllerRef.current) {
+        abortControllerRef.current.abort("canceling previous request");
+      }
+      abortControllerRef.current = new AbortController();
+      dispatch({ type: "unset" }); // probably not needed, but just in case
+      dispatch({ type: "remove" }); // fix for #225 - second import not refreshing view.
+      const response = await fetch(`/api/v2/review/ontopic`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        signal: abortControllerRef.current.signal,
+      });
+      checkReviewResponse(response);
+      return response.json();
+    },
+    onSuccess: (data: OnTopicReviewData) => {
+      setReview(data);
+      if (data.response.html) {
+        dispatch({ type: "update", sentences: data.response.html });
+      }
+    },
+    onSettled: () => {
+      abortControllerRef.current = null;
+    },
+    onError: (error) => {
+      console.error("Error fetching Sentences review:", error);
+      setReview({ tool: "ontopic", error });
+    },
+  });
+  useEffect(() => {
+    if (!document) return;
+    // Fetch the review data for Sentences
+    dispatch({ type: "remove" });
+    mutation.mutate({
+      document,
+    });
+    return () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = null;
+    };
+  }, [document]);
+  useEffect(() => {
+    return () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = null;
+    };
+  }, []);
+  return { review, mutation, setReview, pending: mutation.isPending };
+}
+
 export function useReview<T extends Analysis>(tool: ReviewTool) {
   const [document] = useFileText();
   const { task: writing_task } = useWritingTask();
@@ -227,7 +289,7 @@ export function useReview<T extends Analysis>(tool: ReviewTool) {
       writing_task: Optional<WritingTask>;
     }) => {
       if (abortControllerRef.current) {
-        abortControllerRef.current.abort();
+        abortControllerRef.current.abort("canceling previous request");
       }
       abortControllerRef.current = new AbortController();
       dispatch({ type: "unset" }); // probably not needed, but just in case
@@ -332,6 +394,76 @@ export function useSnapshotReview<T extends Analysis>(
     });
     return () => {
       abortControllerRef.current?.abort();
+    };
+  }, [snapshotID, analysis]);
+  useEffect(() => {
+    return () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = null;
+    };
+  }, []);
+  return { review, mutation, setReview, pending: mutation.isPending };
+}
+
+export function useSnapshotOnTopic(
+  snapshotID: string | undefined,
+  analysis: OptionalReviewData<OnTopicReviewData>
+) {
+  const [review, setReview] =
+    useState<OptionalReviewData<OnTopicReviewData>>(analysis);
+  const dispatch = useReviewDispatch();
+  const abortControllerRef = useRef<AbortController | null>(null);
+  const mutation = useMutation({
+    mutationFn: async (data: { id: string }) => {
+      const { id } = data;
+      if (
+        abortControllerRef.current &&
+        abortControllerRef.current.signal.aborted === false
+      ) {
+        abortControllerRef.current.abort("canceling previous request");
+      }
+      abortControllerRef.current = new AbortController();
+      dispatch({ type: "unset" }); // probably not needed, but just in case
+      dispatch({ type: "remove" }); // fix for #225 - second import not refreshing view.
+      const response = await fetch(`/api/v2/snapshot/${id}/ontopic`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        signal: abortControllerRef.current.signal,
+      });
+      checkReviewResponse(response);
+      return response.json();
+    },
+    onSuccess: (data: OnTopicReviewData) => {
+      setReview(data);
+      if (data.response.html) {
+        dispatch({ type: "update", sentences: data.response.html });
+      }
+    },
+    onError: (error) => {
+      setReview({ tool: "ontopic", error });
+      console.error(`Error fetching Ontopic review:`, error);
+    },
+    onSettled: () => {
+      abortControllerRef.current = null;
+    },
+  });
+  useEffect(() => {
+    if (!snapshotID) return;
+    if (analysis && analysis.tool === "ontopic") {
+      setReview(analysis as OptionalReviewData<OnTopicReviewData>);
+      if ("response" in analysis && analysis.response.html) {
+        dispatch({ type: "update", sentences: analysis.response.html });
+      }
+      return;
+    }
+    mutation.mutate({
+      id: snapshotID,
+    });
+    return () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = null;
     };
   }, [snapshotID, analysis]);
   useEffect(() => {
