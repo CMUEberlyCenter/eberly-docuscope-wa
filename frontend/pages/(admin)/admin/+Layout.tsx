43f5dd533541b7b16cf0fd7369c884681cf84e7a
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-03-11 14:19:22 -0400

    [fix] dependencies and adds underline to active admin page.

--- frontend/pages/(admin)/admin/+Layout.tsx
+++ frontend/pages/(admin)/admin/+Layout.tsx
@@ -2,10 +2,13 @@ import { FC, ReactNode, useId } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { Logo } from "../../../components/Logo/Logo";
+import { usePageContext } from "vike-react/usePageContext";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   const { t } = useTranslation("admin");
   const id = useId();
+  const { urlPathname } = usePageContext();
+  // pageContext.urlParsed.pathname
   return (
     <main className="container-fluid vh-100 w-100 d-flex flex-column align-items-stretch overflow-none">
       <Navbar>
@@ -15,15 +18,23 @@ export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls={id} />
           <Navbar.Collapse id={id}>
-            <Nav className="me-auto">
-              <Nav.Link href="/admin/">{t("home")}</Nav.Link>
-              <Nav.Link href="/admin/settings">{t("settings.tab")}</Nav.Link>
-              <Nav.Link href="/admin/platforms">{t("platforms.tab")}</Nav.Link>
-              <Nav.Link href="/admin/performance">
+            <Nav className="me-auto" variant="underline" defaultActiveKey={"home"}>
+              <Nav.Link active={urlPathname === "/admin/"} href="/admin/" eventKey={"home"}>
+                {t("home")}
+              </Nav.Link>
+              <Nav.Link active={urlPathname === "/admin/settings"} href="/admin/settings">
+                {t("settings.tab")}
+              </Nav.Link>
+              <Nav.Link active={urlPathname === "/admin/platforms"} href="/admin/platforms">
+                {t("platforms.tab")}
+              </Nav.Link>
+              <Nav.Link active={urlPathname === "/admin/performance"} href="/admin/performance">
                 {t("performance.tab")}
               </Nav.Link>
-              <Nav.Link href="/admin/genlink">{t("genlink.tab")}</Nav.Link>
-              <Nav.Link href="/admin/writing_tasks">
+              <Nav.Link active={urlPathname === "/admin/genlink"} href="/admin/genlink">
+                {t("genlink.tab")}
+              </Nav.Link>
+              <Nav.Link active={urlPathname === "/admin/writing_tasks"} href="/admin/writing_tasks">
                 {t("writing_tasks.tab")}
               </Nav.Link>
             </Nav>
