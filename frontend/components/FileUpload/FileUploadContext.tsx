1b14bcd4eeeb59f3878495362b77f7e62d99a495
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-04-08 11:57:21 -0400

    [fix] adds clear file import errors on load. #300
    Moves from effect to callback for processing file which is preferred.

--- frontend/components/FileUpload/FileUploadContext.tsx
+++ frontend/components/FileUpload/FileUploadContext.tsx
@@ -3,7 +3,6 @@ import {
   createContext,
   use,
   useCallback,
-  useEffect,
   useState,
   type FC,
   type ReactNode,
@@ -37,18 +36,59 @@ const loadSaveFileOps = {
 
 /**
  * FileUploadProvider context component for handling essay uploads from the filesystem.
- * @param param0.children - The children to render inside the provider.
- * @returns
  */
 export const FileUploadProvider: FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const { t } = useTranslation();
-  const { showError } = useFileImportErrors();
+  const { clearErrors, showError } = useFileImportErrors();
   const [showUpload, setShowUpload] = useState(false);
   const [, setFilename] = useFilename();
-  const [upload, setUpload] = useState<File | null>(null);
   const [, setText] = useFileText();
+  const processFile = useCallback(
+    (file: File) => {
+      if (!file) {
+        // This should not happen since the file picker only allows selecting files, but we check just in case.
+        setFilename(null);
+        return;
+      }
+      if (
+        file.type !==
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      ) {
+        showError({
+          type: "error",
+          message: t("editor.upload.error.not_docx", { file: file.name }),
+          error: new TypeError(file.name),
+        });
+        return;
+      }
+      clearErrors();
+      setFilename(file.name);
+      file
+        .arrayBuffer()
+        .then((arrayBuffer) =>
+          convertToHtml({ arrayBuffer }, convertOptions).then(
+            ({ value, messages }) => {
+              if (messages.length) {
+                showError(...messages);
+                console.error(messages);
+              }
+              setText(value);
+            }
+          )
+        )
+        .catch((err) => {
+          if (err instanceof Error) {
+            showError({ type: "error", message: err.message, error: err });
+            console.error(err);
+          } else {
+            console.error("Caught non-error", err);
+          }
+        });
+    },
+    [setFilename, setText, showError, t, clearErrors]
+  );
   const uploadFile = useCallback(async () => {
     if (
       "showOpenFilePicker" in window &&
@@ -58,7 +98,7 @@ export const FileUploadProvider: FC<{ children: ReactNode }> = ({
         const [handle]: FileSystemFileHandle[] =
           await window.showOpenFilePicker(loadSaveFileOps);
         const file = await handle.getFile();
-        setUpload(file);
+        processFile(file);
       } catch (err) {
         if (err instanceof DOMException && err.name === "AbortError") {
           return; // Skip cancel.
@@ -76,46 +116,7 @@ export const FileUploadProvider: FC<{ children: ReactNode }> = ({
     } else {
       setShowUpload(true);
     }
-  }, [showError]);
-  useEffect(() => {
-    if (!upload) {
-      setFilename(null);
-      return;
-    }
-    if (
-      upload.type !==
-      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    ) {
-      showError({
-        type: "error",
-        message: t("editor.upload.error.not_docx", { file: upload.name }),
-        error: new TypeError(upload.name),
-      });
-      return;
-    }
-    setFilename(upload.name);
-    upload
-      .arrayBuffer()
-      .then((arrayBuffer) =>
-        convertToHtml({ arrayBuffer }, convertOptions).then(
-          ({ value, messages }) => {
-            if (messages.length) {
-              showError(...messages);
-              console.error(messages);
-            }
-            setText(value);
-          }
-        )
-      )
-      .catch((err) => {
-        if (err instanceof Error) {
-          showError({ type: "error", message: err.message, error: err });
-          console.error(err);
-        } else {
-          console.error("Caught non-error", err);
-        }
-      });
-  }, [upload, showError, t, setFilename, setText]);
+  }, [showError, processFile]);
 
   return (
     <InitiateOpenFileDispatchContext value={uploadFile}>
@@ -123,7 +124,7 @@ export const FileUploadProvider: FC<{ children: ReactNode }> = ({
       <FileUpload
         show={showUpload}
         onHide={() => setShowUpload(false)}
-        onFile={setUpload}
+        onFile={processFile}
       />
     </InitiateOpenFileDispatchContext>
   );
