a331a0a8789ea20c9578411aab7b4434cc7dbf34
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-04-14 10:54:03 -0400

    [enhance] revised deeplink interface without cards or carousel. #298
    Adds custom sliding effect, fixes colors, and uses "small" font.

--- frontend/components/WritingTaskInfo/WritingTaskInfo.tsx
+++ frontend/components/WritingTaskInfo/WritingTaskInfo.tsx
@@ -1,18 +1,25 @@
 import type { WritingTask } from "#/lib/WritingTask";
-import type { FC } from "react";
+import type { FC, HTMLAttributes } from "react";
 import { Translation } from "react-i18next";
 
-type WritingTaskInfoProps = {
+type WritingTaskInfoProps = HTMLAttributes<HTMLDivElement> & {
   /** The writing task to display information about. */
   task: WritingTask | null | undefined;
 };
 /**
  * Component for displaying Metadata about a writing task (outline).
  */
-export const WritingTaskInfo: FC<WritingTaskInfoProps> = ({ task }) => (
+export const WritingTaskInfo: FC<WritingTaskInfoProps> = ({
+  task,
+  className = "bg-light",
+  ...props
+}) => (
   <Translation>
     {(t) => (
-      <div className="border rounded p-3 bg-light w-100 mh-100 overflow-auto">
+      <div
+        className={`border rounded p-3 ${className} w-100 mh-100 overflow-auto`}
+        {...props}
+      >
         <h4>{task?.info.name ?? t("select_task.null")}</h4>
         <p>{task?.rules.overview}</p>
         <p>
