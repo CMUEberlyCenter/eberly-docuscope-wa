bb8cc2e849b24da8d570aec5b6cd5633f4892ee2
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-04-15 14:06:49 -0400

    [fix] moves preview to deeplink and fixes height rendering issue. #298
    Removes preview page and mock dialog.

--- frontend/pages/(admin)/deeplink/+Page.tsx
+++ frontend/pages/(admin)/deeplink/+Page.tsx
@@ -1,73 +1,45 @@
-/* @overview: This is the configuration page for LTI deeplinking. The user can select a writing task from a list, or upload a custom writing task in JSON format. */
+/* @overview: This is the configuration page for LTI deeplinking. The user selects a writing task and a tool. */
 import { WritingTaskFilter } from "#components/WritingTaskFilter/WritingTaskFilter";
 import { WritingTaskInfo } from "#components/WritingTaskInfo/WritingTaskInfo";
-import { validateWritingTask } from "#lib/schemaValidate";
-import { isWritingTask, type WritingTask } from "#lib/WritingTask";
-import { type ChangeEvent, type FC, useId, useRef, useState } from "react";
-import { Form, ListGroup, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { WritingTaskRulesTree } from "#components/WritingTaskRulesTree/WritingTaskRulesTree.js";
+import { type WritingTask } from "#lib/WritingTask";
+import { type FC, useId, useRef, useState } from "react";
+import {
+  Button,
+  Form,
+  ListGroup,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useData } from "vike-react/useData";
 import type { Data } from "./+data";
+import "./style.scss";
 
 export const Page: FC = () => {
   const { t } = useTranslation();
   const { tasks } = useData<Data>();
   const [selected, setSelected] = useState<WritingTask | null>(null);
-  const [custom, setCustom] = useState<WritingTask | null>(null); // Uploaded file content.
-  const [valid, setValid] = useState(true); // Uploaded file validity.
-  const [error, setError] = useState(""); // Error messages for uploaded file.
   const [data, setData] = useState<WritingTask[]>([]);
 
-  const onFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files) {
-      if (files.length > 1) {
-        setValid(false);
-        setError(t("select_task.multiple_file_error"));
-        return;
-      }
-
-      try {
-        const content = await files[0].text();
-        const json = JSON.parse(content);
-        if (!validateWritingTask(json)) {
-          setValid(false);
-          setError(JSON.stringify(validateWritingTask.errors));
-        } else if (isWritingTask(json)) {
-          setValid(true);
-          setCustom(json);
-          setSelected(json);
-        } else {
-          setValid(false);
-          setError(t("select_task.invalid_upload"));
-        }
-      } catch (err) {
-        // expecting JSON parser error.
-        // TODO provide error message to invalid text.
-        setValid(false);
-        console.error(err);
-        if (err instanceof SyntaxError) {
-          setError(t("select_task.malformed_json"));
-        } else {
-          setError(t("select_task.invalid_upload"));
-        }
-      }
-    }
-  };
   const selectId = useId();
   const [tool, setTool] = useState<string>("");
   const targetRef = useRef<HTMLButtonElement>(null);
+  const [index, setIndex] = useState(0);
 
-  // TODO: error page when not in LTI context.
   return (
-    <main className="vh-100 vw-90 p-2">
-      <div className="d-flex flex-column h-100 w-100 gap-3">
+    <main className="h-100 d-flex flex-column overflow-hidden gap-1 p-1 small">
+      <div className="flex-grow-1 minh-0 h-100 overflow-hidden position-relative">
+        {/* <Activity mode={index === 0 ? "visible" : "hidden"}> */}
+        {/* <ViewTransition enter="auto" exit="auto"> available in canary https://react.dev/reference/react/ViewTransition */}
         <div
-          className="d-flex flex-row flex-grow-1 align-items-stretch gap-3 w-100"
-          style={{ minHeight: 0 }}
+          className={`minh-0 h-100 overflow-hidden position-absolute top-0 start-0 d-flex flex-row gap-3 align-items-stretch slide-container start ${index === 0 ? "active" : ""}`}
         >
-          <WritingTaskFilter tasks={tasks} update={setData} />
-          <ListGroup className="overflow-auto w-100">
+          <div className="w-100">
+            <h5 className="fw-bold">{t("select_task.title")}</h5>
+            <WritingTaskFilter tasks={tasks} update={setData} />
+          </div>
+          <ListGroup className="w-100 overflow-auto minh-0 h-100">
             {data
               .toSorted((a, b) => a.info.name.localeCompare(b.info.name))
               .map((task) => (
@@ -80,16 +52,6 @@ export const Page: FC = () => {
                   {task.info.name}
                 </ListGroup.Item>
               ))}
-            {custom && (
-              <ListGroup.Item
-                key={"custom"}
-                action
-                active={selected === custom}
-                onClick={() => setSelected(custom)}
-              >
-                {custom.info.name}
-              </ListGroup.Item>
-            )}
             <ListGroup.Item
               key="null"
               action
@@ -100,48 +62,76 @@ export const Page: FC = () => {
               {t("select_task.null")}
             </ListGroup.Item>
           </ListGroup>
-          <div className="w-100 overflow-auto">
-            <WritingTaskInfo task={selected} />
+          <div className="w-100 h-100 minh-0 d-flex flex-column gap-2 align-items-stretch">
+            <div className="flex-grow-1 minh-0 overflow-hidden">
+              <WritingTaskInfo task={selected} className="" />
+            </div>
+            <div className="d-flex justify-content-end">
+              <Button
+                variant="secondary"
+                disabled={!selected}
+                onClick={() => setIndex(1)}
+              >
+                {t("deeplinking.next")}
+              </Button>
+            </div>
           </div>
         </div>
-        <footer className="d-flex justify-content-between">
-          <Form.Group>
-            <Form.Label>{t("deeplinking.upload")}</Form.Label>
-            <Form.Control
-              type="file"
-              onChange={onFileChange}
-              accept=".json"
-              isInvalid={!valid}
-            />
-            <Form.Control.Feedback type="invalid">
-              {error}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form noValidate method="post" className="d-flex gap-2">
-            <input
-              type="hidden"
-              name="file"
-              value={JSON.stringify(selected)}
-              className="d-none"
-              readOnly={true}
-            />
-            <label htmlFor={selectId} className="">
-              {t("deeplinking.select_tool")}
-              <select
-                id={selectId}
-                className="form-select"
-                name="tool"
-                required
-                onChange={(e) => setTool(e.target.value)}
-                value={tool}
-              >
-                <option value="" selected>
-                  ---
-                </option>
-                <option value="draft">{t("deeplinking.option.draft")}</option>
-                <option value="review">{t("deeplinking.option.review")}</option>
-              </select>
-            </label>
+        {/* </Activity> */}
+        {/* <Activity mode={index === 1 ? "visible" : "hidden"}> */}
+        <div
+          className={`minh-0 h-100 w-100 overflow-hidden position-absolute top-0 start-0 d-flex flex-column gap-3 align-items-stretch slide-container end ${index === 1 ? "active" : ""}`}
+        >
+          <WritingTaskRulesTree
+            detailsClassName="border border-2 rounded"
+            task={selected}
+            includeTitle
+            className="flex-grow-1 minh-0"
+          />
+          <div>
+            <Button
+              variant="secondary"
+              className=""
+              onClick={() => setIndex(0)}
+            >
+              {t("deeplinking.previous")}
+            </Button>
+          </div>
+        </div>
+        {/* </Activity> */}
+      </div>
+      <hr />
+      <div>
+        <Form
+          noValidate
+          method="post"
+          className="d-flex gap-2 justify-content-between align-items-center"
+        >
+          <input
+            type="hidden"
+            name="file"
+            value={JSON.stringify(selected)}
+            className="d-none"
+            readOnly={true}
+          />
+          <label htmlFor={selectId} className="h5 fw-bold w-100">
+            {t("deeplinking.select_tool")}
+          </label>
+          <div className="w-100">
+            <select
+              id={selectId}
+              className="form-select"
+              name="tool"
+              required
+              onChange={(e) => setTool(e.target.value)}
+              value={tool}
+            >
+              <option value="">---</option>
+              <option value="draft">{t("deeplinking.option.draft")}</option>
+              <option value="review">{t("deeplinking.option.review")}</option>
+            </select>
+          </div>
+          <div className="w-100 d-flex justify-content-end">
             <OverlayTrigger
               placement="top"
               overlay={
@@ -168,8 +158,8 @@ export const Page: FC = () => {
                 </button>
               </div>
             </OverlayTrigger>
-          </Form>
-        </footer>
+          </div>
+        </Form>
       </div>
     </main>
   );
--- frontend/pages/(admin)/preview/+title.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageContextServer } from 'vike/types';
-
-export function title(pageContext: PageContextServer) {
-  return (
-    pageContext.i18n?.t('deeplink.title', 'myProse LTI Deeplink') ??
-    'myProse LTI Deeplink'
-  );
-}
