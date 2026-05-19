e5848d40d329994a47aa26d0e10c0f0042898d39
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-05-19 14:50:49 -0400

    [fix] multiple telefunc endpoints.

--- frontend/pages/(admin)/admin/platforms/Page.telefunc.ts
+++ frontend/pages/(admin)/admin/platforms/Page.telefunc.ts
@@ -1,7 +1,9 @@
 import { logger } from '#server/logger.js';
 import { Provider } from 'ltijs';
+import { getAuthorizedUser } from '../getAuthorizedUser';
 
 export async function onActivatePlatform(platformId: string, active: boolean) {
+  getAuthorizedUser();
   try {
     // const { Provider } = await import('ltijs');
     const platform = await Provider.getPlatformById(platformId);
