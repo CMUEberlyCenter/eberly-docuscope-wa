9f74fb127e73ed21968c637379ada62c2c97ede0
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-06-10 15:41:50 -0400

    [fix] #320 adds d-none class to download link.

--- frontend/pages/(admin)/admin/performance/handleDownload.ts
+++ frontend/pages/(admin)/admin/performance/handleDownload.ts
@@ -1,3 +1,8 @@
+/**
+ * Handle downloading data as a JSON file.
+ * @param data stringified JSON data to download
+ * @param fileName desired name of the downloaded file, should end with .json
+ */
 export const handleDownload = (data: string, fileName: string) => {
   const blob = new Blob([data], {
     type: 'application/json',
@@ -6,6 +11,7 @@ export const handleDownload = (data: string, fileName: string) => {
   const a = document.createElement('a');
   a.href = url;
   a.download = fileName;
+  a.classList.add('d-none');
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
