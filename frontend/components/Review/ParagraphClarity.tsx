6fd6edbd9088e5c4dc61adbd552c6a69f47f5fca
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-06-25 09:49:00 -0400

    [fix] #329 accordion highlighting improvements and adjusts accordion header text.

--- frontend/components/Review/ParagraphClarity.tsx
+++ frontend/components/Review/ParagraphClarity.tsx
@@ -61,7 +61,11 @@ export const ParagraphClarity: FC<HTMLProps<HTMLDivElement>> = (props) => {
                   eventKey={`${id}-${i}`}
                 >
                   <Accordion.Header className="accordion-header-highlight">
-                    <div className="flex-grow-1">{issue}</div>
+                    <div className="flex-grow-1">
+                      <h6 className="d-inline">
+                        {t("paragraph_clarity.issue")}
+                      </h6>{" "}
+                      <span>{issue}</span></div>
                     <AlertIcon
                       show={sent_ids.length === 0 && !para_id}
                       message={t("logical_flow.no_sentences")}
