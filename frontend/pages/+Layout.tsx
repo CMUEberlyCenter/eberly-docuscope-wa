2991d3045faca3d74289d95f4af9880db2c04851
Michael Ringenberg <Ringenberg@users.noreply.github.com>
2026-07-08 08:32:04 -0400

    [fix] Improves page transition effect.

--- frontend/pages/+Layout.tsx
+++ frontend/pages/+Layout.tsx
@@ -7,5 +7,5 @@ import { type FC, type ReactNode } from "react";
 import "./index.scss";
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => (
-  <div className="layout">{children}</div>
+  <div className="layout" id="page-content">{children}</div>
 );
